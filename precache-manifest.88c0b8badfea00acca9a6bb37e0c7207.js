781708765ee663ed39504f2169f8ccee63c4205c
Weontack Hong <aosiaos90@gmail.com>
2020-02-02 19:40:02 +0000

    Updates

---
+++ precache-manifest.88c0b8badfea00acca9a6bb37e0c7207.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "503655215889b6f7a740319df0a8959e",
+    "url": "/movie_app_react/index.html"
+  },
+  {
+    "revision": "9fa75cd915c11f91352c",
+    "url": "/movie_app_react/static/css/main.354f3f93.chunk.css"
+  },
+  {
+    "revision": "85097cbf2eed4c5debd7",
+    "url": "/movie_app_react/static/js/2.5f782637.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/movie_app_react/static/js/2.5f782637.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9fa75cd915c11f91352c",
+    "url": "/movie_app_react/static/js/main.917fb159.chunk.js"
+  },
+  {
+    "revision": "ff290e6ea7b92d66cf4e",
+    "url": "/movie_app_react/static/js/runtime-main.5bee4956.js"
+  }
+]);
